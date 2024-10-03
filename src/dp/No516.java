557963798fd1e11361988472a3acb2b9866ac6af
jiangxiaobai1230 <1759445972@qq.com>
2024-10-03 22:31:54 +0800

    leetcode

--- src/dp/No516.java
+++ src/dp/No516.java
@@ -6,9 +6,7 @@ package src.dp;
  */
 public class No516 {
 
-
-
-    public static void main(String[] args) {    public int longestPalindromeSubseq(String s) {
+    public int longestPalindromeSubseq(String s) {
         int len=s.length();
         int[][] dp=new int[len][len];
         for(int i=len-1;i>=0;i--){
@@ -23,6 +21,8 @@ public class No516 {
         return dp[0][len-1];
 
     }
+
+    public static void main(String[] args) {
         //示例 1：
         //
         //输入：s = "bbbab"
